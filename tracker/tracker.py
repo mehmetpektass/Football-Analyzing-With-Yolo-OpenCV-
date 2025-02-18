1d8cfd0585e5c424a6e231129b15d49898e05cd1
mehmetpektass <pektasmehmett1184@gmail.com>
2025-02-18 23:36:03 +0300

    choosed the shape and color of rectangle

--- tracker/tracker.py
+++ tracker/tracker.py
@@ -107,6 +107,27 @@ class Tracker:
         y1_rect = (y2- rectangle_height//2) +15
         y2_rect = (y2+ rectangle_height//2) +15
         
+        if track_id is not None:
+            cv2.rectangle(frame,
+                          (int(x1_rect),int(y1_rect) ),
+                          (int(x2_rect),int(y2_rect)),
+                          color,
+                          cv2.FILLED)
+            
+            x1_text = x1_rect+12
+            if track_id > 99:
+                x1_text -=10
+            
+            cv2.putText(
+                frame,
+                f"{track_id}",
+                (int(x1_text),int(y1_rect+15)),
+                cv2.FONT_HERSHEY_SIMPLEX,
+                0.6,
+                (0,0,0),
+                2
+            )
+        
         return frame
          
             
