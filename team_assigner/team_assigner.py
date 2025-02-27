62f092375ed158f46e0edb1c10110ecf05324e35
mehmetpektass <pektasmehmett1184@gmail.com>
2025-02-27 11:11:10 +0300

    assigned team color and got player team

--- team_assigner/team_assigner.py
+++ team_assigner/team_assigner.py
@@ -2,7 +2,8 @@ from sklearn.cluster import KMeans
 
 class TeamAssigner:
     def __init__(self):
-        pass
+        self.team_colors = {}
+        self.player_team_dict = {}
     
     def get_clustering_model(self, image):
         image_2d = image.reshape(-1,3)
@@ -22,5 +23,44 @@ class TeamAssigner:
         labels = kmeans.labels_
         
         clustered_image = labels.reshape(top_half_of_image.shape[0], top_half_of_image.shape[1])
+        
+        
+        corner_clusters = [clustered_image[0,0], clustered_image[-1,0], clustered_image[0,-1], clustered_image[-1,-1]]
+        non_player_cluster = max(set(corner_clusters), key=corner_clusters.count)
+        player_cluster = 1 - non_player_cluster
+        
+        player_color = kmeans.cluster_centers_(player_cluster)
+        
+        return player_cluster
+    
+    def assign_team_color(self, frame, player_detections):
+        player_colors = []
+        
+        for player_detection in player_detections.items():
+            bbox = player_detection["bbox"]
+            player_color = self.get_player_color(frame, bbox)
+            player_colors.append(player_color)
+            
+        kmeans = KMeans(n_clusters=2, init="k-means++", n_init=1)
+        kmeans.fit(player_colors)
+        
+        self.kmeans =kmeans
+        
+        self.team_colors[0] = kmeans.cluster_centers_[0]
+        self.team_colors[1] = kmeans.cluster_centers_[1]
+        
+                    
+    def get_player_team(self, frame, player_id, player_bbox):
+        player_color = self.get_player_color(frame, player_bbox)
+        
+        team_id = self.kmeans.predict(player_color.reshape(-1,1))[0]
+        team_id =+1
+        
+        self.player_team_dict[player_id] = team_id
+        
+        return team_id
+        
+        
+        
         
         
