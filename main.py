a9994523f58aaa118bf7e095e998c00298d4b386
mehmetpektass <pektasmehmett1184@gmail.com>
2025-02-27 11:25:10 +0300

    commit get team and colors of players functions on the all frames

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from utils import save_video, read_video, crop_image_of_player
 from tracker import Tracker
+from team_assigner import TeamAssigner
 import cv2
 
 def main():
@@ -15,6 +16,19 @@ def main():
    
    # To crap an image of player(If you need one, you should run this code only once)
    #crop_image_of_player(video_frames, tracks)
+   
+   team_assigner = TeamAssigner()
+   team_assigner.assign_team_color(video_frames[0], tracks["players"][0])
+   
+   
+   for frame_num, player_track in enumerate(tracks["players"]):
+      for player_id, track in player_track.item():
+         team = team_assigner.get_player_team(video_frames[frame_num], player_id, track["bbox"])
+         
+         tracks["players"][frame_num][player_id]["team"] = team
+         tracks["players"][frame_num][player_id]["team_color"] = team_assigner.team_colors[team]
+         
+   
    
    output_video_frames = tracker.draw_annotation(video_frames, tracks)
 
