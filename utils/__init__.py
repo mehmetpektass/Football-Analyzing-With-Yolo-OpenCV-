85acd8b492ccbe73c6a46dfd94f10d7301d343cd
mehmetpektass <pektasmehmett1184@gmail.com>
2025-02-28 11:48:59 +0300

    cretaed the function that assigns ball to player

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,3 +1,3 @@
 from .video_utils import save_video, read_video
-from .bbox_utils import get_bbox_width, get_center_of_bbox
+from .bbox_utils import get_bbox_width, get_center_of_bbox, measure_distance
 from .crop_image_utils import crop_image_of_player
