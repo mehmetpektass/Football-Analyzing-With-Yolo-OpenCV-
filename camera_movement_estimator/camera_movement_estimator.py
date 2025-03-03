43d28851640254cba400cb75db3be60fceb14305
mehmetpektass <pektasmehmett1184@gmail.com>
2025-03-04 00:07:16 +0300

    created lk params for the new features

--- camera_movement_estimator/camera_movement_estimator.py
+++ camera_movement_estimator/camera_movement_estimator.py
@@ -4,6 +4,13 @@ import numpy as np
 class CameraMovementEstimator():
     def __init__(self, frame):
         
+        self.lk_params = dict(
+            winSize = (15,15),
+            maxLevel = 2,
+            criteria = (cv2.TERM_CRITERIA_EPS | cv2.TermCriteria_COUNT, 10, 0.03)
+            
+        )
+        
         first_frame_grayscale = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         mask_features = np.zeros_like(first_frame_grayscale)
         mask_features[: , 0:20] = 1
@@ -22,4 +29,8 @@ class CameraMovementEstimator():
         camera_movement = [[0,0]*len(frames)]
         
         old_gray = cv2.cvtColor(frames[0], cv2.COLOR_BGR2GRAY)
-        old_features = cv2.goodFeaturesToTrack(old_features,_)
+        old_features = cv2.goodFeaturesToTrack(old_features,**self.features)
+        
+        for frame_num in range(1, len(frames)):
+            frame_gray = cv2.cvtColor(frames[frame_num], cv2.COLOR_BAYER_BGGR2GRAY)
+            new_features, _ , _ = cv2.calcOpticalFlowPyrLK(old_gray, frame_gray, old_features, None, self.lk_params)
