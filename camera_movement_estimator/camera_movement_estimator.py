34619994cd246f5bbb241d4d54721c7725a0b497
mehmetpektass <pektasmehmett1184@gmail.com>
2025-03-03 23:54:30 +0300

    created features for estimating

--- camera_movement_estimator/camera_movement_estimator.py
+++ camera_movement_estimator/camera_movement_estimator.py
@@ -1,8 +1,21 @@
 import cv2
+import numpy as np
 
 class CameraMovementEstimator():
-    def __init__(self):
-        pass
+    def __init__(self, frame):
+        
+        first_frame_grayscale = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        mask_features = np.zeros_like(first_frame_grayscale)
+        mask_features[: , 0:20] = 1
+        mask_features[: , 900,1050] = 1
+        
+        self.features = dict(
+            max_corners = 100,
+            qualityLevel = 0.3,
+            minDistance = 3,
+            blockSize = 7,
+            mask = mask_features,            
+        )
     
     def get_camera_movement(self,frames, read_from_stub=False, path_stub=None):
         
