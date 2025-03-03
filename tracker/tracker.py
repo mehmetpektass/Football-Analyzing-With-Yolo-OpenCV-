00f8a3ad35a8e89812bce16f0522c66789c1b1d9
mehmetpektass <pektasmehmett1184@gmail.com>
2025-03-03 12:13:10 +0300

    created team ball control rectangle

--- tracker/tracker.py
+++ tracker/tracker.py
@@ -157,8 +157,27 @@ class Tracker:
         cv2.drawContours(frame, [triangle_points],0,(0,0,0), 2)
 
         return frame
-     
     
+    
+    def draw_team_ball_control(self, frame, frame_num, team_ball_control):
+        overlay = frame.copy()
+        
+        cv2.rectangle(overlay, (1350,850), (1900,970), (255,255,255), -1)
+        alpha = 0.4
+        cv2.addWeighted(overlay, alpha, frame, 1- alpha, 0, frame)
+        
+        team_ball_control_till_current_frame = team_ball_control[:frame_num +1]
+        
+        team_1_num_frames = team_ball_control_till_current_frame[team_ball_control_till_current_frame == 1].shape[0]
+        team_2_num_frames = team_ball_control_till_current_frame[team_ball_control_till_current_frame == 2].shape[1]
+        team_1 = team_1_num_frames / (team_1_num_frames + team_2_num_frames)
+        team_2 = team_2_num_frames / (team_1_num_frames + team_2_num_frames)
+        
+        cv2.putText(frame, f"Team 1 Ball Control: {team_1*100:.2f}%", (1400,900), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,0), 3)
+        cv2.putText(frame, f"Team 2 Ball Control: {team_2*100:.2f}%", (1400,950), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,0), 3)
+
+        return frame
+        
     def draw_annotation(self, frames, tracks):
         output_video_frames = []
         
