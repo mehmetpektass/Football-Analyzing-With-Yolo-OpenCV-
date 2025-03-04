4d3371aa0e4bf90593e2e2dd119decf0e8400e2d
mehmetpektass <pektasmehmett1184@gmail.com>
2025-03-04 17:18:08 +0300

    created the function of getting position

--- utils/bbox_utils.py
+++ utils/bbox_utils.py
@@ -10,3 +10,7 @@ def measure_distance(point1, point2):
 
 def measure_xy_distance(point1, point2):
     return point1[0]-point2[0] , point1[1]-point2[1] 
+
+def get_foot_position(bbox):
+    x1, y1, x2, y2 = bbox
+    return (int(x1 + x2)/2), int(y2)
