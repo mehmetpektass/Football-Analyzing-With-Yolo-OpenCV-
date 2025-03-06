f234c43c3248bdf173fa11ccca92b8d949b75732
mehmetpektass <pektasmehmett1184@gmail.com>
2025-03-06 17:49:18 +0300

    created function that draws speed and distance

--- speed_and_distance_estimator/speed_and_distance_estimator.py
+++ speed_and_distance_estimator/speed_and_distance_estimator.py
@@ -1,4 +1,5 @@
-from utils import measure_distance
+from utils import measure_distance, get_foot_position
+import cv2
 
 class SpeedAndDistanceEstimator():
     def __init__(self):
@@ -54,10 +55,19 @@ class SpeedAndDistanceEstimator():
                     continue
                 
                 for _, track_info in object_track[frame_num].items():
-                    if "speed" in track_info
-                    speed = track_info.get("speed", None)
-                    distance = track_info.get("distance", None)
+                    if "speed" in track_info:
+                        speed = track_info.get("speed", None)
+                        distance = track_info.get("distance", None)
                     if speed is None or distance is None:
                         continue
                     
+                    bbox = track_info["bbox"]
+                    position = get_foot_position(bbox)
+                    position = list(position)
+                    position = position[1]+40
                     
+                    position = tuple(map(int, position))
+                    cv2.putText(frame, f"{speed:.2f} km/h", position, cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,0), 2)
+                    cv2.putText(frame, f"{distance:.2f} m", (position[0], position[1]+20), cv2.FONT_HERSHEY_SIMPLEX, (0,0,0), 0.5, 2)
+            output_frames.append(frame)
+        return output_frames 
